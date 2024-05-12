948298c857435182c3bc094ce9ff02f72610ae12
PDT <phamduytung2002@gmail.com>
2024-05-12 23:04:14 +0700

    update

--- topmost/evaluations/topic_coherence.py
+++ topmost/evaluations/topic_coherence.py
@@ -50,7 +50,7 @@ def C_V_on_wikipedia(top_word_path, cv_type='C_V'):
     wiki_dir = os.path.join(".", 'data')
     random_number = np.random.randint(100000)
     os.system(
-        f'java -jar {os.path.join(jar_dir, 'pametto.jar')} {os.path.join(wiki_dir, 'wikipedia', 'wikipedia_bd')} {cv_type} {top_word_path} > tmp{random_number}.txt')
+        f"java -jar {os.path.join(jar_dir, 'pametto.jar')} {os.path.join(wiki_dir, 'wikipedia', 'wikipedia_bd')} {cv_type} {top_word_path} > tmp{random_number}.txt")
     cv_score = []
     with open(f"tmp{random_number}.txt", "r") as f:
         for line in f.readlines():
