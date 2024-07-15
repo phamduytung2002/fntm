7cdb994c324b3d6082266a2967546ce887dfd83a
PDT <phamduytung2002@gmail.com>
2024-07-15 11:22:48 +0700

    add setupenv.sh

---
+++ setupenv.sh
@@ -0,0 +1,10 @@
+conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=11.8 -c pytorch  -c nvidia -y
+pip install torch_kmeans
+pip install matplotlib
+pip install wandb
+pip install sentence_transformers==2.2.2
+pip install gensim
+pip install scipy==1.10.1
+conda install -c conda-forge openjdk -y
+conda install -c conda-forge hdbscan -y
+pip install bertopic
